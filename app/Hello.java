a2248028278c673f257d144974f233c0360e5adf
Dhiraj Mahale <dhirajmahale2@gmail.com>
2024-01-11 11:46:52 +0530

    added app folder in main branch

---
+++ app/Hello.java
@@ -0,0 +1,12 @@
+package com.first.code;
+
+public class Hello {
+
+	public static void main(String[] args) {
+		System.out.println("Hello JAVA");
+		System.err.println(" my name is  dhiraj mahale.");
+		
+
+	}
+
+}
